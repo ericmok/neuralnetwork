b18e99c94169f73ea089fa8c313aa0e929fa138c
Eric M <eric.mok.0@gmail.com>
2014-09-02 14:58:22 -0400

    Added tests

---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var chai = require('chai'),
+    should = chai.should(),
+    assert = chai.assert;
+
+describe('Test', function() {
+    it('works', function(done) {
+        assert.equal(true, true);
+        done();
+    });
+});
