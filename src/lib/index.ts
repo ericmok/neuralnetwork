01f8edb41dfb07877ff08c1bdbf4915d1509c030
Eric M <ericmok2@gmail.com>
2016-10-04 23:36:06 -0400

    Removed code comments.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,28 +1,6 @@
 /*
-This lets browserify squash everything into a single file
-with easy access to frequently used dependencies.
+A single file interface for entire library
 */
-// function require(a: string) : any {}
-//
-// module.exports = {
-//     'network': require('./network'),
-//     'Network': require('./network').Network,
-//     'meanSquaredError': require('./network').meanSquaredError,
-//
-//     'layers': require('./layers'),
-//     'Layer': require('./layers').Layer,
-//
-//     'neurons': require('./neurons'),
-//     'BiasNeuron': require('./neurons').BiasNeuron,
-//     'IdentityNeuron': require('./neurons').IdentityNeuron,
-//     'SigmoidNeuron': require('./neurons').SigmoidNeuron,
-//     'RectifiedLinearNeuron': require('./neurons').RectifiedLinearNeuron,
-//
-//     'connections': require('./connections'),
-//     'FullConnection': require('./connections').FullConnection,
-//
-//     'utils': require('./utils')
-// };
 
 import * as utils from './utils';
 export { utils };
