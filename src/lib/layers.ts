f4ac3cf7d110be9ae8d77a49274d41a56db7d8a6
Eric M <ericmok2@gmail.com>
2016-10-04 21:50:58 -0400

    Removed comments

--- src/lib/layers.ts
+++ src/lib/layers.ts
@@ -8,32 +8,6 @@ export interface LayerDefinition {
 }
 
 /**
-Instead of copying functions, we maintain an indirection to it instead
-*/
-// function ifObjectThenCreateDefaultNeuron(obj: N): N {
-//     'use strict';
-//
-//     var key, // temp
-//         indirection,
-//         ret;
-//
-//     if (typeof obj === 'function') {
-//         ret = new obj();
-//         return ret;
-//     }
-//
-//     indirection = new neurons.Neuron();
-//     for (key in obj) {
-//         if (obj.hasOwnProperty(key)) {
-//             indirection[key] = obj[key];
-//         }
-//     }
-//
-//     return indirection;
-//}
-
-
-/**
 Constructor to create a new layer of neurons
 */
 export class Layer {
@@ -65,16 +39,6 @@ export class Layer {
                 this.neurons.push(new args[i].kind());
             }
         }
-
-        //var neuronType : N | null = null;
-
-        // for (let i = 0; i < args.length / 2; i += 1) {
-        //     for (let j = 0; j < args[i * 2 + 1].amount; j += 1) {
-        //         //neuronType = ifObjectThenCreateDefaultNeuron(args[i * 2]);
-        //
-        //         this.neurons.push(neuronType);
-        //     }
-        // }
     }
 
     resetBuffers() {
@@ -103,58 +67,3 @@ export class Layer {
         });
     }
 }
-/*
-Layer.prototype.resetBuffers = function () {
-    'use strict';
-    var i;
-
-    for (i = 0; i < this.neurons.length; i += 1) {
-        this.inputBuffer[i] = 0;
-        this.outputBuffer[i] = 0;
-        this.inputError[i] = 0;
-        this.outputError[i] = 0;
-    }
-};
-
-Layer.prototype.createMixNeurons = function (args) {
-    'use strict';
-
-    var i, j,
-        neuronType = null;
-
-    for (i = 0; i < args.length / 2; i += 1) {
-
-        for (j = 0; j < args[i * 2 + 1]; j += 1) {
-            neuronType = ifObjectThenCreateDefaultNeuron(args[i * 2]);
-            this.neurons.push(neuronType);
-        }
-    }
-};
-
-Layer.prototype.forward = function () {
-    'use strict';
-
-    var self = this;
-
-    this.neurons.forEach(function (el, index, arr) {
-        self.outputBuffer[index] = el.forward(self.inputBuffer[index]);
-    });
-
-};
-
-Layer.prototype.backward = function () {
-    'use strict';
-
-    var self = this;
-
-    this.neurons.forEach(function (el, index, arr) {
-        // TODO: Test this
-        self.inputError[index] = el.backward(self.outputError[index], self.outputBuffer[index]);
-    });
-
-};
-
-module.exports = {
-    'Layer': Layer
-};
-*/
